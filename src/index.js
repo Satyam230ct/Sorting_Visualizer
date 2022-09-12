ebfc5d5313727feb2e5493d07b00eb37dd7d605e
Satyam Kumar <satyam@YoYo-MPro.local>
2022-09-12 21:55:32 +0530

    added utilities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SortingVisualizer from "./SortingVisualizer/SortingVisualizer.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import { useGlobalState, Context } from './Components/Uitlities/utils'
 
-// ## Rendering SortingVisualizer Component ( Imported from path mentioned above ) ## //
-ReactDOM.render(<SortingVisualizer />, document.getElementById("root"));
+// const Index = () => {
+// 	const store = useGlobalState();
+// 	return (
+// 		<Context.Provider value={store}>
+// 			<App />
+// 		</Context.Provider>
+// 	)
+// }
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
